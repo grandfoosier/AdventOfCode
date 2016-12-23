5a902faa3384cad9e4c932cbeef965a60e41b51b
Steve Rice <rice.steve@gmail.com>
2016-12-23 10:27:21 -0500

    Create AoC_23_2.py

---
+++ 2016/23/AoC_23_2.py
@@ -0,0 +1,11 @@
+from math import factorial
+
+def calcA(a):
+    A = factorial(a) + 75 * 71
+    return A
+
+#######################################################################
+# Main Routine
+print "\n", calcA(7)
+print "\n", calcA(12)
+print "\n\n"
