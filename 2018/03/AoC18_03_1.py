326f6a3e8d4ba8fc67dc0f73dc690f6430621e06
Steve Rice <rice.steve@gmail.com>
2018-12-03 18:59:43 -0500

    Create AoC18_03_1.py

---
+++ 2018/03/AoC18_03_1.py
@@ -0,0 +1,23 @@
+import re
+
+def run():
+    FabA = [[0] * 1000 for i in range(1000)]
+    for line in input:
+        sp = re.split('[@,:x]', line)
+        x,y=int(sp[1]),int(sp[2])
+        a,b=int(sp[3]),int(sp[4])
+        for r in range(y, y + b):
+            for c in range(x, x + a):
+                FabA[r][c] += 1
+    overlap = 0
+    for r in range(1000):
+        for c in range(1000):
+            if FabA[r][c] > 1: overlap += 1
+    print('Overlap:', overlap)
+
+if __name__ == '__main__':
+    with open('AoC18_03_1.txt') as file:
+        input = [line.rstrip('\n') for line in file]
+    print('Input Loaded\n')
+    run()
+    print('\n')
